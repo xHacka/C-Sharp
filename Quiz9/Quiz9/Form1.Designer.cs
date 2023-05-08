1d557fa371b1d19a9de430b3a566bf8b7a000ebf
xHacka <pvpgamerpro457@gmail.com>
2023-05-08 17:00:51 +0400

    Quiz9

---
+++ Quiz9/Quiz9/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Quiz9 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.task1Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.task1Input = new System.Windows.Forms.TextBox();
+            this.task1Output = new System.Windows.Forms.Label();
+            this.task1ArrayOutput = new System.Windows.Forms.Label();
+            this.task2Input_i = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.task2Output = new System.Windows.Forms.Label();
+            this.task2ArrayOutput = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.task2Btn = new System.Windows.Forms.Button();
+            this.task2Input_j = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.task3Btn = new System.Windows.Forms.Button();
+            this.task3Output = new System.Windows.Forms.Label();
+            this.task4Btn = new System.Windows.Forms.Button();
+            this.task4Output = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.task5Btn = new System.Windows.Forms.Button();
+            this.task5Output = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // task1Btn
+            // 
+            this.task1Btn.Location = new System.Drawing.Point(15, 102);
+            this.task1Btn.Name = "task1Btn";
+            this.task1Btn.Size = new System.Drawing.Size(99, 23);
+            this.task1Btn.TabIndex = 0;
+            this.task1Btn.Text = "Show Number";
+            this.task1Btn.UseVisualStyleBackColor = true;
+            this.task1Btn.Click += new System.EventHandler(this.task1Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Index: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Task 1";
+            // 
+            // task1Input
+            // 
+            this.task1Input.Location = new System.Drawing.Point(86, 57);
+            this.task1Input.Name = "task1Input";
+            this.task1Input.Size = new System.Drawing.Size(28, 20);
+            this.task1Input.TabIndex = 2;
+            // 
+            // task1Output
+            // 
+            this.task1Output.AutoSize = true;
+            this.task1Output.Location = new System.Drawing.Point(12, 81);
+            this.task1Output.Name = "task1Output";
+            this.task1Output.Size = new System.Drawing.Size(0, 13);
+            this.task1Output.TabIndex = 1;
+            // 
+            // task1ArrayOutput
+            // 
+            this.task1ArrayOutput.AutoSize = true;
+            this.task1ArrayOutput.Location = new System.Drawing.Point(13, 38);
+            this.task1ArrayOutput.Name = "task1ArrayOutput";
+            this.task1ArrayOutput.Size = new System.Drawing.Size(34, 13);
+            this.task1ArrayOutput.TabIndex = 1;
+            this.task1ArrayOutput.Text = "Array:";
+            // 
+            // task2Input_i
+            // 
+            this.task2Input_i.Location = new System.Drawing.Point(334, 141);
+            this.task2Input_i.Name = "task2Input_i";
+            this.task2Input_i.Size = new System.Drawing.Size(28, 20);
+            this.task2Input_i.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Task 2";
+            // 
+            // task2Output
+            // 
+            this.task2Output.AutoSize = true;
+            this.task2Output.Location = new System.Drawing.Point(260, 165);
+            this.task2Output.Name = "task2Output";
+            this.task2Output.Size = new System.Drawing.Size(0, 13);
+            this.task2Output.TabIndex = 5;
+            // 
+            // task2ArrayOutput
+            // 
+            this.task2ArrayOutput.AutoSize = true;
+            this.task2ArrayOutput.Location = new System.Drawing.Point(260, 38);
+            this.task2ArrayOutput.Name = "task2ArrayOutput";
+            this.task2ArrayOutput.Size = new System.Drawing.Size(41, 13);
+            this.task2ArrayOutput.TabIndex = 6;
+            this.task2ArrayOutput.Text = "Matrix: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(260, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Enter Index: ";
+            // 
+            // task2Btn
+            // 
+            this.task2Btn.Location = new System.Drawing.Point(263, 186);
+            this.task2Btn.Name = "task2Btn";
+            this.task2Btn.Size = new System.Drawing.Size(133, 23);
+            this.task2Btn.TabIndex = 3;
+            this.task2Btn.Text = "Show Number";
+            this.task2Btn.UseVisualStyleBackColor = true;
+            this.task2Btn.Click += new System.EventHandler(this.task2Btn_Click);
+            // 
+            // task2Input_j
+            // 
+            this.task2Input_j.Location = new System.Drawing.Point(368, 141);
+            this.task2Input_j.Name = "task2Input_j";
+            this.task2Input_j.Size = new System.Drawing.Size(28, 20);
+            this.task2Input_j.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(458, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Task 3";
+            // 
+            // task3Btn
+            // 
+            this.task3Btn.Location = new System.Drawing.Point(461, 134);
+            this.task3Btn.Name = "task3Btn";
+            this.task3Btn.Size = new System.Drawing.Size(133, 23);
+            this.task3Btn.TabIndex = 3;
+            this.task3Btn.Text = "Show Number";
+            this.task3Btn.UseVisualStyleBackColor = true;
+            this.task3Btn.Click += new System.EventHandler(this.task3Btn_Click);
+            // 
+            // task3Output
+            // 
+            this.task3Output.AutoSize = true;
+            this.task3Output.Location = new System.Drawing.Point(465, 57);
+            this.task3Output.Name = "task3Output";
+            this.task3Output.Size = new System.Drawing.Size(0, 13);
+            this.task3Output.TabIndex = 5;
+            // 
+            // task4Btn
+            // 
+            this.task4Btn.Location = new System.Drawing.Point(34, 344);
+            this.task4Btn.Name = "task4Btn";
+            this.task4Btn.Size = new System.Drawing.Size(133, 23);
+            this.task4Btn.TabIndex = 3;
+            this.task4Btn.Text = "Write To File";
+            this.task4Btn.UseVisualStyleBackColor = true;
+            this.task4Btn.Click += new System.EventHandler(this.task4Btn_Click);
+            // 
+            // task4Output
+            // 
+            this.task4Output.AutoSize = true;
+            this.task4Output.Location = new System.Drawing.Point(38, 318);
+            this.task4Output.Name = "task4Output";
+            this.task4Output.Size = new System.Drawing.Size(0, 13);
+            this.task4Output.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 280);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Task 4";
+            // 
+            // task5Btn
+            // 
+            this.task5Btn.Location = new System.Drawing.Point(263, 394);
+            this.task5Btn.Name = "task5Btn";
+            this.task5Btn.Size = new System.Drawing.Size(133, 23);
+            this.task5Btn.TabIndex = 3;
+            this.task5Btn.Text = "Write To File";
+            this.task5Btn.UseVisualStyleBackColor = true;
+            this.task5Btn.Click += new System.EventHandler(this.task5Btn_Click);
+            // 
+            // task5Output
+            // 
+            this.task5Output.AutoSize = true;
+            this.task5Output.Location = new System.Drawing.Point(266, 318);
+            this.task5Output.Name = "task5Output";
+            this.task5Output.Size = new System.Drawing.Size(0, 13);
+            this.task5Output.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(259, 280);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Task 5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.task2Input_j);
+            this.Controls.Add(this.task2Input_i);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.task5Output);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.task4Output);
+            this.Controls.Add(this.task3Output);
+            this.Controls.Add(this.task2Output);
+            this.Controls.Add(this.task2ArrayOutput);
+            this.Controls.Add(this.task5Btn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.task4Btn);
+            this.Controls.Add(this.task3Btn);
+            this.Controls.Add(this.task2Btn);
+            this.Controls.Add(this.task1Input);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.task1Output);
+            this.Controls.Add(this.task1ArrayOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.task1Btn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button task1Btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox task1Input;
+        private System.Windows.Forms.Label task1Output;
+        private System.Windows.Forms.Label task1ArrayOutput;
+        private System.Windows.Forms.TextBox task2Input_i;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label task2Output;
+        private System.Windows.Forms.Label task2ArrayOutput;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button task2Btn;
+        private System.Windows.Forms.TextBox task2Input_j;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button task3Btn;
+        private System.Windows.Forms.Label task3Output;
+        private System.Windows.Forms.Button task4Btn;
+        private System.Windows.Forms.Label task4Output;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button task5Btn;
+        private System.Windows.Forms.Label task5Output;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Quiz9/Quiz9/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz9 {
+    public partial class Form1 : Form {
+        Random rng = new Random();
+
+        public int[] GenerateRandomArray(int size, int max = 100) {
+            return Enumerable.Range(1, size).Select(_ => rng.Next(max)).ToArray();
+        }
+        const int task2ArraySize = 5;
+        int[] task1Array;
+        int[,] task2Array = new int[task2ArraySize, task2ArraySize];
+
+        public Form1() {
+            InitializeComponent();
+            String tab = new string(' ', 10);
+            task1Array = GenerateRandomArray(10);
+            task1ArrayOutput.Text = $"Array: {String.Join(" ", task1Array)}";
+            task2ArrayOutput.Text = $"Matrix: \n{tab}";
+            for (int i = 0; i < task2ArraySize; i++) {
+                int[] row = GenerateRandomArray(task2ArraySize);
+                for (int j = 0; j < task2ArraySize; j++) {
+                    String padding = row[j].ToString().Length > 1 ? new string(' ', 3) : new string(' ', 5);
+                    task2Array[i, j] = row[j];
+                    task2ArrayOutput.Text += $"{row[j]}{padding}";
+                }
+                task2ArrayOutput.Text += $"\n{tab}";
+            }
+        }
+
+        private void task1Btn_Click(object sender, EventArgs e) {
+            /// ?
+            // int sum = 0;
+            // for (int i = 0; i < task1Array.Length; i++) {
+            //     try { sum += task1Array[i]; } 
+            //     catch (IndexOutOfRangeException) { task1Output.Text = "Invalid Index!"; return; }
+            // }
+            // return sum;
+            int index;
+
+            try { index = int.Parse(task1Input.Text.Trim()); } 
+            catch (FormatException) { task1Output.Text = "Input Not A Number!"; return; }
+
+            try { task1Output.Text = $"Index {index} = {task1Array[index]}"; } 
+            catch (IndexOutOfRangeException) { task1Output.Text = "Invalid Index!"; return; }
+        }
+
+        private void task2Btn_Click(object sender, EventArgs e) {
+            /// ?
+            // int sum = 0;
+            // for (int i = 0; i < task2Array.GetLength(0); i++) {
+            //     for (int j = 0; j < task2Array.GetLength(1); j++) {
+            //         try { sum += task2Array[i, j]; }
+            //         catch (IndexOutOfRangeException) { task2Output.Text = "Invalid Indexes!"; return; }
+            //     }
+            // }
+            // return sum;
+            int i, j;
+
+            try { i = int.Parse(task2Input_i.Text.Trim()); j = int.Parse(task2Input_j.Text.Trim()); } 
+            catch (FormatException) { task2Output.Text = "Invalid Input! (Must Be A Number)"; return; }
+
+            try { task2Output.Text = $"task2Array[{i}, {j}] = {task2Array[i, j]}"; } 
+            catch (IndexOutOfRangeException) { task2Output.Text = "Invalid Indexes!"; return; }
+        }
+
+        private void task3Btn_Click(object sender, EventArgs e) {
+            task3Output.Text = "";
+            const int SIZE = 5;
+            int[,] array1 = new int[SIZE, SIZE], array2 = new int[SIZE, SIZE];
+            for (int i = 0; i < SIZE; i++) { 
+                for (int j = 0; j < SIZE; j++) {  
+                    array1[i, j] = rng.Next(101); 
+                    array2[i, j] = rng.Next(26); 
+                }  
+            }
+
+            int x = 0, y = 0;
+            for (int i = 0; i < array1.GetLength(0); i++) { // Intentionally Creating Bug
+                for (int j = 0; j < array1.GetLength(1) + (rng.Next(7) == 4 ? rng.Next(3) : 0); j++) {
+                    try {
+                        x = array1[i, j]; 
+                        y = array2[i, j];
+                        if (x == 0 || y == 0) {
+                            task3Output.Text += ( x / y).ToString("0.00") + "   ";                    
+                        } else {
+                            task3Output.Text += ( x / (float) y).ToString("0.00") + "   ";                    
+                        }
+                    } catch (IndexOutOfRangeException) {
+                        task3Output.Text += $"\n --- Error: Index Out Of Range i={i}, j={j} ---\n"; return;
+                    } catch (DivideByZeroException) {
+                        task3Output.Text += $"\n --- Error: Zero Division x={x} y={y} ---\n"; return;
+                    }
+                }
+                task3Output.Text += "\n";
+            }
+        }
+
+        private void task4Btn_Click(object sender, EventArgs e) {
+            task4Output.Text = "";
+            String filename = "task4.txt";
+            try { // Write
+                using (FileStream file = File.Open(filename, FileMode.Create)) {
+                    using (BinaryWriter writer = new BinaryWriter(file)) {
+                        foreach (int number in GenerateRandomArray(10)) {
+                            writer.Write(number);
+                        }
+                    }
+                }
+            } catch (IOException err) {
+                task4Output.Text += $"\nError While Trying To Write To File: {err.Message}";
+            }
+
+            try { // Read
+                using (FileStream file = File.Open(filename, FileMode.Open)) {
+                    using (BinaryReader reader = new BinaryReader(file)) {
+                        while (reader.PeekChar() != -1) {
+                            task4Output.Text += $"{reader.ReadInt32()} ";   
+                        }
+                    }
+                }
+            } catch (IOException err) {
+                task4Output.Text += $"\nError While Trying To Read File: {err.Message}";
+            }
+
+        }
+
+        private void task5Btn_Click(object sender, EventArgs e) {
+            task5Output.Text = "";
+            String filename = "task5.txt";
+            try { // Write
+                using (FileStream file = File.Open(filename, FileMode.Create)) {
+                    using (StreamWriter writer = new StreamWriter(file)) {
+                        for (int i = 0; i < 5; i++) {
+                            foreach (int number in GenerateRandomArray(5)) {
+                                writer.Write($"{number} ");
+                            }
+                            writer.Write("\n");
+                        }
+                    }
+                }
+            } catch (IOException err) {
+                task5Output.Text += $"\nError While Trying To Write To File: {err.Message}";
+            }
+
+            try { // Read
+                using (FileStream file = File.Open(filename, FileMode.Open)) {
+                    using (StreamReader reader = new StreamReader(file)) {
+                        while (!reader.EndOfStream) {
+                            task5Output.Text += $"{reader.ReadLine()}\n";   
+                        }
+                    }
+                }
+            } catch (IOException err) {
+                task5Output.Text += $"\nError While Trying To Read File: {err.Message}";
+            }
+
+        }
+    }
+}
