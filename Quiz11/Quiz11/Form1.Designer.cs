37dad8e7ebfb283f69dc689f4ce909ecc0e5d057
xHacka <pvpgamerpro457@gmail.com>
2023-06-18 17:17:55 +0400

    La Qviz 11

---
+++ Quiz11/Quiz11/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Quiz11 {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            TABLE = new DataGridView();
+            viewTableBtn = new Button();
+            filterBtn = new Button();
+            orderByBtn = new Button();
+            findBtn = new Button();
+            femalesCntBtn = new Button();
+            maxSalaryBtn = new Button();
+            minSalaryBtn = new Button();
+            avgSalaryBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)TABLE).BeginInit();
+            SuspendLayout();
+            // 
+            // TABLE
+            // 
+            TABLE.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            TABLE.Location = new Point(8, 8);
+            TABLE.Name = "TABLE";
+            TABLE.RowTemplate.Height = 25;
+            TABLE.Size = new Size(886, 400);
+            TABLE.TabIndex = 0;
+            // 
+            // viewTableBtn
+            // 
+            viewTableBtn.Location = new Point(18, 429);
+            viewTableBtn.Name = "viewTableBtn";
+            viewTableBtn.Size = new Size(75, 23);
+            viewTableBtn.TabIndex = 1;
+            viewTableBtn.Text = "View Table";
+            viewTableBtn.UseVisualStyleBackColor = true;
+            viewTableBtn.Click += viewTableBtn_Click;
+            // 
+            // filterBtn
+            // 
+            filterBtn.Location = new Point(114, 429);
+            filterBtn.Name = "filterBtn";
+            filterBtn.Size = new Size(75, 23);
+            filterBtn.TabIndex = 1;
+            filterBtn.Text = "Filter";
+            filterBtn.UseVisualStyleBackColor = true;
+            filterBtn.Click += filterBtn_Click;
+            // 
+            // orderByBtn
+            // 
+            orderByBtn.Location = new Point(215, 429);
+            orderByBtn.Name = "orderByBtn";
+            orderByBtn.Size = new Size(75, 23);
+            orderByBtn.TabIndex = 2;
+            orderByBtn.Text = "Order";
+            orderByBtn.UseVisualStyleBackColor = true;
+            orderByBtn.Click += orderByBtn_Click;
+            // 
+            // findBtn
+            // 
+            findBtn.Location = new Point(316, 429);
+            findBtn.Name = "findBtn";
+            findBtn.Size = new Size(75, 23);
+            findBtn.TabIndex = 2;
+            findBtn.Text = "Find";
+            findBtn.UseVisualStyleBackColor = true;
+            findBtn.Click += findBtn_Click;
+            // 
+            // femalesCntBtn
+            // 
+            femalesCntBtn.Location = new Point(417, 429);
+            femalesCntBtn.Name = "femalesCntBtn";
+            femalesCntBtn.Size = new Size(75, 23);
+            femalesCntBtn.TabIndex = 2;
+            femalesCntBtn.Text = "Females";
+            femalesCntBtn.UseVisualStyleBackColor = true;
+            femalesCntBtn.Click += femalesCntBtn_Click;
+            // 
+            // maxSalaryBtn
+            // 
+            maxSalaryBtn.Location = new Point(516, 429);
+            maxSalaryBtn.Name = "maxSalaryBtn";
+            maxSalaryBtn.Size = new Size(75, 23);
+            maxSalaryBtn.TabIndex = 2;
+            maxSalaryBtn.Text = "Max Salary";
+            maxSalaryBtn.UseVisualStyleBackColor = true;
+            maxSalaryBtn.Click += maxSalaryBtn_Click;
+            // 
+            // minSalaryBtn
+            // 
+            minSalaryBtn.Location = new Point(610, 429);
+            minSalaryBtn.Name = "minSalaryBtn";
+            minSalaryBtn.Size = new Size(75, 23);
+            minSalaryBtn.TabIndex = 2;
+            minSalaryBtn.Text = "Min Salary";
+            minSalaryBtn.UseVisualStyleBackColor = true;
+            minSalaryBtn.Click += minSalaryBtn_Click;
+            // 
+            // avgSalaryBtn
+            // 
+            avgSalaryBtn.Location = new Point(709, 429);
+            avgSalaryBtn.Name = "avgSalaryBtn";
+            avgSalaryBtn.Size = new Size(75, 23);
+            avgSalaryBtn.TabIndex = 2;
+            avgSalaryBtn.Text = "Avg Salary";
+            avgSalaryBtn.UseVisualStyleBackColor = true;
+            avgSalaryBtn.Click += avgSalaryBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(906, 514);
+            Controls.Add(avgSalaryBtn);
+            Controls.Add(minSalaryBtn);
+            Controls.Add(maxSalaryBtn);
+            Controls.Add(femalesCntBtn);
+            Controls.Add(findBtn);
+            Controls.Add(orderByBtn);
+            Controls.Add(filterBtn);
+            Controls.Add(viewTableBtn);
+            Controls.Add(TABLE);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            ((System.ComponentModel.ISupportInitialize)TABLE).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView TABLE;
+        private Button viewTableBtn;
+        private Button filterBtn;
+        private Button orderByBtn;
+        private Button findBtn;
+        private Button femalesCntBtn;
+        private Button maxSalaryBtn;
+        private Button minSalaryBtn;
+        private Button avgSalaryBtn;
+    }
+}
---
+++ Quiz11/Quiz11/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace Quiz11 {
+    public partial class Form1 : Form {
+        //SqlConnection dbConnection = new SqlConnection("server=BTU306-L\\SQLEXPRESS;database=Shekveta;uid=sa;pwd=1");
+        SqlConnection dbConnection = new SqlConnection(@"
+            Data Source=DESKTOP-JUQCHVR\SQLEXPRESS;
+            Initial Catalog=Shekveta;
+            Integrated Security=True
+        ");
+
+        public Form1() {
+            dbConnection.Open();
+            InitializeComponent();
+            viewTableBtn_Click(null, null);
+        }
+
+        private DataTable RunQuery(String sql) {
+            SqlCommand query = new SqlCommand(sql, dbConnection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        private void viewTableBtn_Click(object sender, EventArgs e) {
+            String tableName = "Shemkveti";
+            DataTable dataTable = RunQuery($"SELECT * FROM {tableName}");
+            TABLE.DataSource = dataTable;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            dbConnection.Close();
+        }
+
+        private void filterBtn_Click(object sender, EventArgs e) {
+            String tableName = "Shemkveti";
+            String cityFilter = "qalaqi = N'თელავი'";
+            String legalEntityTypeFilter = "iuridiuli_fizikuri = N'იურიდიული'";
+            DataTable dataTable = RunQuery($@"
+                 SELECT * FROM {tableName}
+                 WHERE {cityFilter} AND {legalEntityTypeFilter}
+            ");
+            TABLE.DataSource = dataTable;
+        }
+
+        private void orderByBtn_Click(object sender, EventArgs e) {
+            String tableName = "Xelshekruleba";
+            String orderBy = "tarigi_dawyebis DESC, gadasaxdeli_l ASC";
+
+            DataTable dataTable = RunQuery($@"
+                select * from {tableName}
+                order by {orderBy}
+            ");
+            TABLE.DataSource = dataTable;
+
+            /// or
+            // DataTable dataTable = RunQuery($"SELECT * FROM {tableName}");
+            // DataView dataView = dataTable.DefaultView;
+            // dataView.Sort = orderBy;
+            // TABLE.DataSource = dataView;
+        }
+
+        private void findBtn_Click(object sender, EventArgs e) {
+            String tableName = "Personali";
+            String pid = "4";
+            String findBy = $"personaliID = {pid}";
+
+            DataTable dataTable = RunQuery($"SELECT * FROM {tableName} WHERE {findBy}");
+            TABLE.DataSource = dataTable;
+
+            // DataTable dataTable = RunQuery($"SELECT * FROM {tableName}");
+            // DataColumn[] keys = { dataTable.Columns["PersonaliID"] };
+            // dataTable.PrimaryKey = keys;
+            // DataRow row = dataTable.Rows.Find(pid);
+            // if (row is null) { MessageBox.Show("No Row Found By ID=4"); }
+            // else { DataTable tmp = dataTable.Clone(); tmp.ImportRow(row); TABLE.DataSource = tmp; };
+        }
+
+        private void femalesCntBtn_Click(object sender, EventArgs e) {
+            String tableName = "Personali as p";
+            String joinTable = "Shemkveti as o ON p.personaliID = o.personaliID";
+            String filterBy = "p.sqesi = N'ქალი' AND o.iuridiuli_fizikuri = N'ფიზიკური'";
+            DataTable dataTable = RunQuery($@"
+                SELECT p.* FROM {tableName} 
+                JOIN {joinTable}
+                WHERE {filterBy}
+            ");
+            TABLE.DataSource = dataTable;
+            MessageBox.Show($"Found {dataTable.Rows.Count} Female Individual Records");
+        }
+
+        private void maxSalaryBtn_Click(object sender, EventArgs e) {
+            String tableName = "Personali";
+            DataTable dataTable = RunQuery($"SELECT * FROM {tableName}");
+            object maxSalaryQueryResult = dataTable.Compute("Max(xelfasi)", "");
+            decimal maxSalary = Convert.ToDecimal(maxSalaryQueryResult);
+            MessageBox.Show($"Max Salary: ${maxSalary}");
+        }
+
+        private void minSalaryBtn_Click(object sender, EventArgs e) {
+            String tableName = "Personali";
+            DataTable dataTable = RunQuery($"SELECT * FROM {tableName}");
+            object minSalaryQueryResult = dataTable.Compute("Min(xelfasi)", "");
+            decimal minSalary = Convert.ToDecimal(minSalaryQueryResult);
+            MessageBox.Show($"Min Salary: ${minSalary}");
+        }
+
+        private void avgSalaryBtn_Click(object sender, EventArgs e) {
+            String tableName = "Personali";
+            DataTable dataTable = RunQuery($"SELECT * FROM {tableName}");
+            object avgSalaryQueryResult = dataTable.Compute("Avg(xelfasi)", "ganyofileba = 'სავაჭრო'");
+            decimal avgSalary = Convert.ToDecimal(avgSalaryQueryResult);
+            MessageBox.Show($"Marketing Average Salary: ${avgSalary}");
+        }
+    }
+}
